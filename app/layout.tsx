77776a0ec6760b0958e625cd3f24233560ea5a2e
Taufeeq28 <sameerahmed6686@gmail.com>
2024-10-08 22:22:35 -0500

    skills added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 
-import "./globals.css";
+import './css/globals.scss';
 import Navbar from './components/navbar';
+import './css/card.scss';
 
 // Import Inter font if needed
 import { Inter } from 'next/font/google';
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/homepage/hero-section";
 import AboutSection from "./components/homepage/about-section";
 import Experience from "./components/homepage/experience";
+import Skills from "./components/homepage/skills";
 export default function Home() {
   return (<>
   <HeroSection/>
   <AboutSection />
   <Experience />
+  <Skills/>
   </>)
 }
